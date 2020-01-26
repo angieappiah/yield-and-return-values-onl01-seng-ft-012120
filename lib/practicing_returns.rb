56b705d6884894355dfcf7e40d25ff70d027a68c
Angela Appiah <alegnalawn2@yahoo.com>
2020-01-26 23:26:53 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -2,11 +2,12 @@
 
 def hello(array)
   i = 0
-  collection = [ ]
+  collection = [ "{Tim}", "{Tom}", "{Jim}" ]
   while i < array.length
-  collection.push(yield(array[i])) 
+  collection.push (yield(array[i])) 
     i += 1
   end
+end
   #p collection / testing if it works collection
 end
 
